2715df7e1c94523d5a29a89a4086bcf6ce36857c
pacifique <pacifiquenyiringango1998@gmail.com>
2022-02-23 23:50:35 +0200

    setup postgres PR review

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import logger from 'morgan'
+<<<<<<< HEAD
 import { sequelize } from '../server/models/index'
+=======
+>>>>>>> a0086c9 (setup postgres PR review)
 import swaggerJsDoc from 'swagger-jsdoc'
 import swaggerUI from 'swagger-ui-express'
 import bodyParser from 'body-parser'
 
 // Required Routes
+<<<<<<< HEAD
+=======
+import welcomeRoute from './routes/welcomeRoute'
+import userRoute from './routes/user'
+>>>>>>> a0086c9 (setup postgres PR review)
 
 import welcomeRoute from './routes/welcomeRoute'
 import user from './routes/user'
@@ -24,6 +32,7 @@ if (app.get('env') === 'development') {
   app.use(logger('dev'))
   console.log('Morgan logger is enabled...')
 }
+<<<<<<< HEAD
 app.all('*', function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS, DELETE, GET')
@@ -35,6 +44,19 @@ app.all('*', function (req, res, next) {
 app.use(bodyParser.json({ limit: '100mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: 'true' }))
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+=======
+app.all('*', function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+  res.header("Access-Control-Max-Age", "3600");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-access-token");
+  next();
+});
+
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb','extended': 'true'}));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+>>>>>>> a0086c9 (setup postgres PR review)
 
 // Swagger Info Object
 const swaggerOptions = {
@@ -54,6 +76,7 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions)
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
+<<<<<<< HEAD
 // Use routes
 app.use(welcomeRoute)
 app.use(user)
@@ -61,6 +84,14 @@ app.use(login)
 // port & hostname
 const port = process.env.APP_PORT || 3000
 const hostname = 'localhost'
+=======
+// Custom Middleware
+app.use(welcomeRoute)
+app.use(userRoute)
+
+// PORT
+const port = process.env.APP_PORT || 3000
+>>>>>>> a0086c9 (setup postgres PR review)
 
 // Listening to requests
 app.listen(port, async () => {
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Users from '../controllers/user'
 import express from 'express'
 
@@ -15,3 +16,28 @@ router.put('/users/update/:id', Users.updateUser)
 router.delete('/users/delete/:id', Users.deleteUser)
 
 export default router
+=======
+import express from 'express'
+import Users from '../controllers/user'
+
+const route = express.Router()
+
+// Welcome endpoint
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    description: Used to display the Welcome Page
+ *    responses:
+ *      '200':
+ *        description: The Welcome Page was displayed successfully
+ */
+
+  route.post('/api/users', Users.create);
+  route.get('/api/users', Users.listAll);
+  route.get('/api/users/:id', Users.list);
+  route.put('/api/users/:id', Users.modify);
+  route.delete('/api/users/:id', Users.delete);
+
+export default route
+>>>>>>> a0086c9 (setup postgres PR review)
