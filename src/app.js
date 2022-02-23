a0086c94b18ea859b66a32efa7686e343dbbebd2
pacifique <pacifiquenyiringango1998@gmail.com>
2022-02-23 23:50:35 +0200

    setup postgres PR review

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import logger from 'morgan';
-import swaggerJsDoc from 'swagger-jsdoc';
-import swaggerUI from 'swagger-ui-express';
-import config from '../config/config.js';
+import express from 'express'
+import logger from 'morgan'
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUI from 'swagger-ui-express'
+import bodyParser from 'body-parser'
 
 // Required Routes
-import welcomeRoute from './routes/welcomeRoute';
+import welcomeRoute from './routes/welcomeRoute'
+import userRoute from './routes/user'
 
 // Initialize express app
 const app = express();
@@ -15,6 +16,17 @@ if (app.get('env') === 'development') {
   app.use(logger('dev'));
   console.log('Morgan logger is enabled...');
 }
+app.all('*', function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+  res.header("Access-Control-Max-Age", "3600");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-access-token");
+  next();
+});
+
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb','extended': 'true'}));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
 // Swagger Info Object
 const swaggerOptions = {
@@ -35,10 +47,11 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 // Custom Middleware
-app.use(welcomeRoute);
+app.use(welcomeRoute)
+app.use(userRoute)
 
 // PORT
-const port = config.app.port || 3000;
+const port = process.env.APP_PORT || 3000
 
 // Listening to requests
 app.listen(port, () => {
