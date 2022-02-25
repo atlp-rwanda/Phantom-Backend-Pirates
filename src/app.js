1f9cb18f383386af37daf6f16cadcb69e9674d6e
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-02-25 15:25:15 +0200

    ft-supporting multiple languages

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import logger from 'morgan'
 import swaggerJsDoc from 'swagger-jsdoc'
 import swaggerUI from 'swagger-ui-express'
 import config from '../config/config.js'
-
 // Required Routes
 import welcomeRoute from './routes/welcomeRoute'
 
+import i18next from 'i18next'
+import i18nextMiddleware from 'i18next-express-middleware'
 // Initialize express app
 const app = express()
+
+// telling Express to use i18next's middleware
+app.use(i18nextMiddleware.handle(i18next))
 
 // Morgan for the logger in the console
 if (app.get('env') === 'development') {
@@ -38,7 +42,7 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 app.use(welcomeRoute)
 
 // PORT
-const port = config.app.port
+const port = config.app.port || 3000
 
 // Listening to requests
 app.listen(port, () => {
