e345d18cf910672538e8874d744fbd761f0404ef
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-02-25 15:25:15 +0200

    ft-supporting multiple languages

--- test/test.js
+++ test/test.js
@@ -1,29 +1,65 @@
-const chai = require('chai');
-const chaiHTTP = require('chai-http');
-const { app } = require('../src/app');
+const chai = require('chai')
+const chaiHTTP = require('chai-http')
+const { app } = require('../src/app')
+const expect = chai.expect()
 
-chai.should();
-chai.use(chaiHTTP);
+chai.should()
+chai.use(chaiHTTP)
 
 describe('server connection', () => {
   describe('server connected', () => {
-    it('it Should show return an html page', (done) => {
+    
+    it('welcomes user to the api in english/default', (done) => {
       chai
         .request(app)
         .get('/')
         .end((err, res) => {
           res.should.have.status(200);
+          res.body.message.should.be.equal('Welcome to Phantom Web');
+          res.body.error.should.be.equal(false);
           done();
         });
     });
-    it('Home page should have Welcome text', (done) => {
+
+    it('welcomes user to the api in french', (done) => {
       chai
         .request(app)
-        .get('/')
+        .get('/?lng=fr')
         .end((err, res) => {
-          res.text.should.be.equal('<h1>Welcome to Phantom Web App!!!!!</h1>');
+          res.should.have.status(200);
+          res.body.message.should.be.equal('Bienvenue sur le Web de Phantom');
+          res.body.error.should.be.equal(false);
+
           done();
         });
     });
+
+    it('welcomes user to the api in Kinyarwanda', (done) => {
+      chai
+        .request(app)
+        .get('/?lng=rw')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.message.should.be.equal("Ikaze k'Urubuga rwa Phantom");
+          res.body.error.should.be.equal(false);
+          done();
+        });
+    });
+
+    it('404 Error', (done) => {
+      chai
+        .request(app)
+        .get('/random')
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+
+
+
+
+
+
   });
 });
