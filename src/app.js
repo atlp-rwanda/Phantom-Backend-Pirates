6a2bf8858edf8eb515808941d4b58af0b85ea335
pacifique <pacifiquenyiringango1998@gmail.com>
2022-02-28 22:19:27 +0200

    setup project postgres

--- src/app.js
+++ src/app.js
@@ -1,24 +1,19 @@
 import express from 'express'
 import logger from 'morgan'
-<<<<<<< HEAD
-import { sequelize } from '../server/models/index'
-=======
->>>>>>> a0086c9 (setup postgres PR review)
+import { sequelize } from '../models/index'
 import swaggerJsDoc from 'swagger-jsdoc'
 import swaggerUI from 'swagger-ui-express'
 import bodyParser from 'body-parser'
+import dotenv from "dotenv"
+
+dotenv.config();
 
 // Required Routes
-<<<<<<< HEAD
-=======
 import welcomeRoute from './routes/welcomeRoute'
 import userRoute from './routes/user'
->>>>>>> a0086c9 (setup postgres PR review)
-
 import welcomeRoute from './routes/welcomeRoute'
 import user from './routes/user'
 import login from './routes/login'
-
 import i18next from 'i18next'
 import i18nextMiddleware from 'i18next-express-middleware'
 // Initialize express app
@@ -26,13 +21,15 @@ const app = express()
 
 // telling Express to use i18next's middleware
 app.use(i18nextMiddleware.handle(i18next))
-
 // Morgan for the logger in the console
 if (app.get('env') === 'development') {
   app.use(logger('dev'))
   console.log('Morgan logger is enabled...')
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> afe8fbb (setup project postgres)
 app.all('*', function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS, DELETE, GET')
@@ -40,6 +37,7 @@ app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-access-token')
   next()
 })
+<<<<<<< HEAD
 
 app.use(bodyParser.json({ limit: '100mb' }))
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: 'true' }))
@@ -57,6 +55,12 @@ app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb','extended': 'true'}));
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 >>>>>>> a0086c9 (setup postgres PR review)
+=======
+
+app.use(bodyParser.json({ limit: '100mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: 'true' }))
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+>>>>>>> afe8fbb (setup project postgres)
 
 // Swagger Info Object
 const swaggerOptions = {
@@ -87,17 +91,21 @@ const hostname = 'localhost'
 =======
 // Custom Middleware
 app.use(welcomeRoute)
-app.use(userRoute)
 
 // PORT
 const port = process.env.APP_PORT || 3000
 >>>>>>> a0086c9 (setup postgres PR review)
 
 // Listening to requests
+<<<<<<< HEAD
 app.listen(port, async () => {
   console.log(`Server running at http://${hostname}:${port}/..`)
   await sequelize.authenticate()
   console.log('Databse connected successfully')
+=======
+app.listen(port, () => {
+  console.log(`Server running on port ${port}..... `)
+>>>>>>> afe8fbb (setup project postgres)
 })
 
 export { app }
---
+++ test/test.js
@@ -0,0 +1,67 @@
+const chai = require('chai')
+const chaiHTTP = require('chai-http')
+const { app } = require('../src/app')
+<<<<<<< HEAD
+<<<<<<< HEAD
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const { app } = require('../src/app');
+=======
+>>>>>>> 44a261d (cheking if husky works)
+=======
+>>>>>>> afe8fbb (setup project postgres)
+
+chai.should()
+chai.use(chaiHTTP)
+
+describe('server connection', () => {
+  describe('server connected', () => {
+    it('welcomes user to the api in english/default', (done) => {
+      chai
+        .request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200)
+          done()
+        })
+    })
+    it('Home page should have Welcome text', (done) => {
+      chai
+        .request(app)
+        .get('/?lng=fr')
+        .end((err, res) => {
+<<<<<<< HEAD
+          res.should.have.status(200)
+          res.body.message.should.be.equal('Bienvenue sur le Web de Phantom')
+          res.body.error.should.be.equal(false)
+
+          done()
+        })
+    })
+
+    it('welcomes user to the api in Kinyarwanda', (done) => {
+      chai
+        .request(app)
+        .get('/?lng=rw')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.message.should.be.equal("Ikaze k'Urubuga rwa Phantom")
+          res.body.error.should.be.equal(false)
+          done()
+        })
+    })
+
+    it('404 Error', (done) => {
+      chai
+        .request(app)
+        .get('/random')
+        .end((err, res) => {
+          res.should.have.status(404)
+=======
+>>>>>>> afe8fbb (setup project postgres)
+          res.text.should.be.equal('<h1>Welcome to Phantom Web App!!!!!</h1>')
+          done()
+        })
+    })
+  })
+})
