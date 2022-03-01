3e4afe58e592d7a8bda4ccace1986277bcbad3cc
John Niyontwali <nijohn0006@gmail.com>
2022-03-01 04:47:35 +0200

    Testing Husky

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,17 @@
 import login from '../controllers/login'
 import express from 'express'
 
-const router = express.Router()
+    const router = express.Router()
 
 // End-point for user to login
 router.post('/users/login', login)
 
 export default router
+
+
+
+
+
+
+
+
