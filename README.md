47f46361158f8994b4c9431da203719de72d49ae
pacifique <pacifiquenyiringango1998@gmail.com>
2022-03-01 20:16:24 +0200

    Sign in to Phantom commits
    husky trial 1
    
    Husky trial trial2
    
    Husky trial trial 2
    
    husky trial 3
    
    husky trial 1
    
    husky rebased
    
    husky trial 1
    
    Husky trial trial2
    
    Husky trial trial 2
    
    husky trial 3
    
    husky trial 1
    
    Husky trial trial2
    
    husky trial 3
    
    Linting and configurations
    
    Worked on the setup of the project structure,
    welcome endpoint, configured the ESLint for code analysis
    and setup the API documentation with Swagger
    
    I made the following changes:
    1. Added script for linting
    2. Added development script which uses nodemon
    3. Added production script that uses node
    4. Removed the styles on the Welcome package
    5. Added a controllers folder to handle all controllers for the routes
    
    I have added babel tool to the project in order
    to use new ES features
    
    I removed a file that contained the commit messages.
    
    removed the plugins in the .eslintrc.js file
    
    Made some changes on the eslint configuration set up
    
    dotenv setup
    
    chore(dotenv): configuration of environments
    
    dotenv setup
    
    config forlder created & config.js modification
    
    configure CI/CD using Github Actions and Heroku
    
    github action secret keys added
    
    update README.md
    
    conflicts resolved
    
    chai-http package added
    
    problem of accessing app function resolved
    
    running test errors solved
    
    chore-Creation of Node container
    
    Revert "chore-Creation of Node container"
    
    ft-supporting multiple languages
    
    ft-supporting multiple languages
    
    removed the app.spec.js file
    
    setup postgres PR review
    
    setup postgres PR review
    
    setup project postgres
    
    setup project postgres
    
    resolve readme
    
    resolve
    
    husky trial 1
    
    Husky trial trial2
    
    Husky trial trial 2
    
    husky trial 3
    
    husky trial 1
    
    husky rebased
    
    husky trial 1
    
    Husky trial trial2
    
    Husky trial trial 2
    
    husky trial 3
    
    husky trial 1
    
    Husky trial trial 2
    
    husky trial 3
    
    Signing to phantom
    
    Removing License file
    
    rebasing with develop
    
    Made the following changes and modifications
    - Modified the resources folder in order to support multiple languages
    - Modified the login controller to support multiple languages
    - Deleted the user route and controller since am using dummy data
    
    Modified the login.js and removed the misplacements
    
    The following changes were made:
    - modified the resources/locales/ folder
    - renamed the constants related to multi-language in order to match with
    the names in the modified folder (resources/locales)
    
    The following changes were made:
    - added coverage and nyc package and edited the test script
    - editted wording in translations
    - Added more tests
    
    Made the following changes and modifications:
    - modified my tests
    - Modified seeders and migrations
    
    Added Swagger Login API Documentation
    
    Modified the documentation for the Login API
    
    rebase
    
    squash

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Phantom-Backend-Pirates
 
+<<<<<<< HEAD
 [![CI](https://github.com/atlp-rwanda/Phantom-Backend-Pirates/actions/workflows/main.yml/badge.svg?branch=develop)](https://github.com/atlp-rwanda/Phantom-Backend-Pirates/actions/workflows/main.yml)
+=======
+>>>>>>> 1abd4ee (resolve)
--- test/login.js
+++ test/login.js
@@ -10,154 +10,138 @@ describe('Login API', () => {
   describe('/users/login', () => {
     it('A registered user should be able to login/default language', (done) => {
       const user = {
-        email: 'jane@gmail.com',
-        password: '123456',
-      };
+        email:"jane@gmail.com",
+        password:"123456"
+      }
       chai
         .request(app)
         .post('/users/login')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(200);
-          res.body.message.should.be.equal(
-            'You have successfully logged in as an Admin'
-          );
-          done();
-        });
-    });
+          res.should.have.status(200)
+          res.body.message.should.be.equal('You have successfully logged in as an Operator')
+          done()
+        })
+    })
     it('A registered user should be able to login/Kinyarwanda Language', (done) => {
       const user = {
-        email: 'jane@gmail.com',
-        password: '123456',
-      };
+        email:"jane@gmail.com",
+        password:"123456"
+      }
       chai
         .request(app)
         .post('/users/login/?lng=rw')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(200);
-          res.body.message.should.be.equal('Winjiye neza nka Admin');
-          done();
-        });
-    });
+          res.should.have.status(200)
+          res.body.message.should.be.equal('Winjiye neza nka Operator')
+          done()
+        })
+    })
     it('A registered user should be able to login/French Language', (done) => {
       const user = {
-        email: 'jane@gmail.com',
-        password: '123456',
-      };
+        email:"jane@gmail.com",
+        password:"123456"
+      }
       chai
         .request(app)
         .post('/users/login/?lng=fr')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(200);
-          res.body.message.should.be.equal(
-            "Vous vous êtes connecté avec succès en tant qu'administrateur"
-          );
-          done();
-        });
-    });
+          res.should.have.status(200)
+          res.body.message.should.be.equal("Vous vous êtes connecté avec succès en tant qu'opérateur")
+          done()
+        })
+    })
     it('A non-registered user shouldnot be able to login/eDfault Language', (done) => {
       const user = {
-        email: 'nijohn@gmail.com',
-        password: 'holdon',
-      };
+        email:"nijohn@gmail.com",
+        password:"holdon"
+      }
       chai
         .request(app)
         .post('/users/login')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(400);
-          res.body.error.should.be.equal(
-            'The email is not registered! Please first register'
-          );
-          done();
-        });
-    });
+          res.should.have.status(400)
+          res.body.error.should.be.equal('The email is not registered! Please first register')
+          done()
+        })
+    })
     it('A non-registered user shouldnot be able to login/Kinyarwanda Language', (done) => {
       const user = {
-        email: 'nijohn@gmail.com',
-        password: 'holdon',
-      };
+        email:"nijohn@gmail.com",
+        password:"holdon"
+      }
       chai
         .request(app)
         .post('/users/login/?lng=rw')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(400);
-          res.body.error.should.be.equal(
-            'Imeri yanditswe ntabwo tuyifite! Nyamuneka banza wiyandikishe'
-          );
-          done();
-        });
-    });
+          res.should.have.status(400)
+          res.body.error.should.be.equal("Imeri yanditswe ntabwo tuyifite! Nyamuneka banza wiyandikishe")
+          done()
+        })
+    })
     it('A non-registered user shouldnot be able to login/French Language', (done) => {
       const user = {
-        email: 'nijohn@gmail.com',
-        password: 'holdon',
-      };
+        email:"nijohn@gmail.com",
+        password:"holdon"
+      }
       chai
         .request(app)
         .post('/users/login/?lng=fr')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(400);
-          res.body.error.should.be.equal(
-            "L'e-mail n'est pas enregistré ! Veuillez d'abord vous inscrire"
-          );
-          done();
-        });
-    });
+          res.should.have.status(400)
+          res.body.error.should.be.equal("L'e-mail n'est pas enregistré ! Veuillez d'abord vous inscrire")
+          done()
+        })
+    })
     it('A registered user with wrong password logins shouldnot be able to login/Default Language', (done) => {
       const user = {
-        email: 'jane@gmail.com',
-        password: 'holdon',
-      };
+        email:"jane@gmail.com",
+        password:"holdon"
+      }
       chai
         .request(app)
         .post('/users/login')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(401);
-          res.body.error.should.be.equal(
-            'The email or passwords entered is wrong'
-          );
-          done();
-        });
-    });
+          res.should.have.status(401)
+          res.body.error.should.be.equal("The email or passwords entered is wrong")
+          done()
+        })
+    })
     it('A registered user with wrong password logins shouldnot be able to login/Kinyarwanda Language', (done) => {
       const user = {
-        email: 'jane@gmail.com',
-        password: 'holdon',
-      };
+        email:"jane@gmail.com",
+        password:"holdon"
+      }
       chai
         .request(app)
         .post('/users/login/?lng=rw')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(401);
-          res.body.error.should.be.equal(
-            'Imeri cyangwa ijambo ryibanga ryinjiye sibyo'
-          );
-          done();
-        });
-    });
+          res.should.have.status(401)
+          res.body.error.should.be.equal("Imeri cyangwa ijambo ryibanga ryinjiye sibyo")
+          done()
+        })
+    })
     it('A registered user with wrong password logins shouldnot be able to login/French Language', (done) => {
       const user = {
-        email: 'jane@gmail.com',
-        password: 'holdon',
-      };
+        email:"jane@gmail.com",
+        password:"holdon"
+      }
       chai
         .request(app)
         .post('/users/login/?lng=fr')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(401);
-          res.body.error.should.be.equal(
-            "L'e-mail ou les mots de passe saisis sont incorrects"
-          );
-          done();
-        });
-    });
-  });
-});
+          res.should.have.status(401)
+          res.body.error.should.be.equal("L'e-mail ou les mots de passe saisis sont incorrects")
+          done()
+        })
+    })
+  })
+})
