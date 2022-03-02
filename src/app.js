cd730929cef602383f302e498e2a66bb75b85093
John Niyontwali <nijohn0006@gmail.com>
2022-03-02 19:05:23 +0200

    The following changes were made: -login test was established

--- src/app.js
+++ src/app.js
@@ -31,13 +31,8 @@ app.all('*', function (req, res, next) {
   next()
 })
 
-app.all('*', function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS, DELETE, GET')
-  res.header('Access-Control-Max-Age', '3600')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-access-token')
-  next()
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // Swagger Info Object
 const swaggerOptions = {
--- test/login.js
+++ test/login.js
@@ -8,13 +8,48 @@ chai.use(chaiHttp)
 describe('Login API', () => {
   // Testing login end-point
   describe('/users/login', () => {
-    it('It should allow an authenticated user to login', (done) => {
+    it('A registered user should be able to login', (done) => {
+      const user = {
+        email:"jane@gmail.com",
+        password:"123456"
+      }
       chai
         .request(app)
-        .get('/users/login')
+        .post('/users/login')
+        .send(user)
         .end((err, res) => {
           res.should.have.status(200)
-
+          res.body.message.should.be.equal('Operator successfully logged in')
+          done()
+        })
+    })
+    it('A non-registered user shouldnot be able to login', (done) => {
+      const user = {
+        email:"nijohn@gmail.com",
+        password:"holdon"
+      }
+      chai
+        .request(app)
+        .post('/users/login')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.body.error.should.be.equal('The email is not registered! Please first register')
+          done()
+        })
+    })
+    it('A registered user with wrong password logins shouldnot be able to login', (done) => {
+      const user = {
+        email:"jane@gmail.com",
+        password:"holdon"
+      }
+      chai
+        .request(app)
+        .post('/users/login')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(401)
+          res.body.error.should.be.equal("The passwords entered don't match")
           done()
         })
     })
