90d0f376c1309b58117a9c38d89fd112293ef53b
John Niyontwali <nijohn0006@gmail.com>
2022-03-02 16:38:23 +0200

    Made the following changes -created a dummy user and used bcrypt to hash the password -modified imports in both controllers and routes since my config folder is in the root directory

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Users from '../controllers/user'
 import express from 'express'
 
@@ -16,28 +15,3 @@ router.put('/users/update/:id', Users.updateUser)
 router.delete('/users/delete/:id', Users.deleteUser)
 
 export default router
-=======
-import express from 'express'
-import Users from '../controllers/user'
-
-const route = express.Router()
-
-// Welcome endpoint
-/**
- * @swagger
- * /:
- *  get:
- *    description: Used to display the Welcome Page
- *    responses:
- *      '200':
- *        description: The Welcome Page was displayed successfully
- */
-
-  route.post('/api/users', Users.create);
-  route.get('/api/users', Users.listAll);
-  route.get('/api/users/:id', Users.list);
-  route.put('/api/users/:id', Users.modify);
-  route.delete('/api/users/:id', Users.delete);
-
-export default route
->>>>>>> a0086c9 (setup postgres PR review)
