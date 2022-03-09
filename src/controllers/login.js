1227e71acb434d455d27ce10becd539b8275e135
John Niyontwali <nijohn0006@gmail.com>
2022-03-09 15:43:00 +0200

    pulled from origin develop

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,57 +1,57 @@
-import { User } from '../../models';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
+import { User } from '../../models'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const login =
   ('/',
   (req, res) => {
-    const { email, password } = req.body;
+    const { email, password } = req.body
     // Responses for translations
-    const responseAdmin = req.t('login_message.onSuccess.admin');
-    const responseOperator = req.t('login_message.onSuccess.operator');
-    const responseDriver = req.t('login_message.onSuccess.driver');
+    const responseAdmin = req.t('login_message.onSuccess.admin')
+    const responseOperator = req.t('login_message.onSuccess.operator')
+    const responseDriver = req.t('login_message.onSuccess.driver')
     const responseUnsuccessNonregistered = req.t(
       'login_message.onFailure.nonregistered'
-    );
+    )
     const responseUnsuccessWrongPass = req.t(
       'login_message.onFailure.wrongpassword'
-    );
+    )
     User.findOne({
       where: {
-        email,
-      },
+        email
+      }
     })
       .then((user) => {
         if (!user) {
           res.status(400).json({
-            error: `${responseUnsuccessNonregistered}`,
-          });
+            error: `${responseUnsuccessNonregistered}`
+          })
         } else {
           const userRole = {
             isAdmin: true,
             isOperator: false,
-            isDriver: false,
-          };
+            isDriver: false
+          }
           if (bcrypt.compareSync(password, user.password) && userRole.isAdmin) {
             const adminToken = jwt.sign(
               { user: user },
               process.env.ADMIN_SECRET,
               {
-                expiresIn: process.env.AUTH_EXPIRES,
+                expiresIn: process.env.AUTH_EXPIRES
               }
-            );
+            )
             res.cookie('jwt', adminToken, {
               httpOnly: true,
-              expiresIn: process.env.AUTH_EXPIRES,
-            });
+              expiresIn: process.env.AUTH_EXPIRES
+            })
             res.status(200).json({
               message: `${responseAdmin}`,
               admin: [user.name, user.email],
-              adminToken,
-            });
+              adminToken
+            })
           } else if (
             bcrypt.compareSync(password, user.password) &&
             userRole.isOperator
@@ -60,18 +60,18 @@ const login =
               { user: user },
               process.env.OPERATOR_SECRET,
               {
-                expiresIn: process.env.AUTH_EXPIRES,
+                expiresIn: process.env.AUTH_EXPIRES
               }
-            );
+            )
             res.cookie('jwt', operatorToken, {
               httpOnly: true,
-              expiresIn: process.env.AUTH_EXPIRES,
-            });
+              expiresIn: process.env.AUTH_EXPIRES
+            })
             res.status(200).json({
               message: `${responseOperator}`,
               operator: [user.name, user.email],
-              operatorToken,
-            });
+              operatorToken
+            })
           } else if (
             bcrypt.compareSync(password, user.password) &&
             userRole.isDriver
@@ -80,27 +80,27 @@ const login =
               { user: user },
               process.env.DRIVER_SECRET,
               {
-                expiresIn: process.env.AUTH_EXPIRES,
+                expiresIn: process.env.AUTH_EXPIRES
               }
-            );
+            )
             res.cookie('jwt', driverToken, {
               httpOnly: true,
-              expiresIn: process.env.AUTH_EXPIRES,
-            });
+              expiresIn: process.env.AUTH_EXPIRES
+            })
             res.status(200).json({
               message: `${responseDriver}`,
               driver: [user.name, user.email],
-              driverToken,
-            });
+              driverToken
+            })
           } else {
             res.status(401).json({
-              error: `${responseUnsuccessWrongPass}`,
-            });
+              error: `${responseUnsuccessWrongPass}`
+            })
           }
         }
       })
       .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
-export default login;
+        res.status(500).json(err)
+      })
+  })
+export default login
