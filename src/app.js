af40dfb1a7f5309e13f431ec935efa9d4b49bee2
christian <habinezac15@gmail.com>
2022-03-25 15:07:58 +0200

    port corrected

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(cookieParser());
 
 // Port & hostname
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 const hostname = process.env.HOST_NAME;
 // Swagger Info Object
 const swaggerOptions = {
