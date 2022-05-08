0815eda70f6e198fe5e4f4cb5921fd3ddf60b934
pacifique <pacifiquenyiringango1998@gmail.com>
2022-05-08 11:31:37 +0200

    bg-fix-calls: change headers origin

--- src/app.js
+++ src/app.js
@@ -48,13 +48,14 @@ var origin;
 //const allowedOrigins = ['http://localhost:4000', `${process.env.FRONTEND_URL}`];
 app.use(function (req, res, next) {
   origin = req.headers.origin;
+  console.log("==========",origin)
   //if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', `${origin}`);
  // }
  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'POST, PUT, OPTIONS, DELETE, GET');
   res.header('Access-Control-Max-Age', '3600');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers") 
   return next();
 });
 
